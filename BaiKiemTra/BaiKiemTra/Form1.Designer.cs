fd9b41ff2b9889235c3af34abad77b744acc27f4
yoralong <yoralong@gmail.com>
2022-03-08 09:53:34 +0700

    Thanh công

--- BaiKiemTra/BaiKiemTra/Form1.Designer.cs
+++ BaiKiemTra/BaiKiemTra/Form1.Designer.cs
@@ -38,54 +38,59 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(113, 104);
+            this.label1.Location = new System.Drawing.Point(99, 78);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 20);
+            this.label1.Size = new System.Drawing.Size(85, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tên đăng nhập";
             // 
             // textboxusername
             // 
-            this.textboxusername.Location = new System.Drawing.Point(235, 104);
+            this.textboxusername.Location = new System.Drawing.Point(206, 78);
+            this.textboxusername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textboxusername.Name = "textboxusername";
-            this.textboxusername.Size = new System.Drawing.Size(125, 27);
+            this.textboxusername.Size = new System.Drawing.Size(110, 23);
             this.textboxusername.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(113, 195);
+            this.label2.Location = new System.Drawing.Point(99, 146);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.Size = new System.Drawing.Size(57, 15);
             this.label2.TabIndex = 0;
             this.label2.Text = "Mật khẩu";
             // 
             // textboxmatkhau
             // 
-            this.textboxmatkhau.Location = new System.Drawing.Point(235, 195);
+            this.textboxmatkhau.Location = new System.Drawing.Point(206, 146);
+            this.textboxmatkhau.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textboxmatkhau.Name = "textboxmatkhau";
-            this.textboxmatkhau.Size = new System.Drawing.Size(125, 27);
+            this.textboxmatkhau.Size = new System.Drawing.Size(110, 23);
             this.textboxmatkhau.TabIndex = 1;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(373, 295);
+            this.btnLogin.Location = new System.Drawing.Point(326, 221);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(94, 29);
+            this.btnLogin.Size = new System.Drawing.Size(82, 22);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "Đăng nhập";
             this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.textboxmatkhau);
             this.Controls.Add(this.textboxusername);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- BaiKiemTra/BaiKiemTra/Form1.cs
+++ BaiKiemTra/BaiKiemTra/Form1.cs
@@ -2,11 +2,25 @@ namespace BaiKiemTra
 {
     public partial class Form1 : Form
     {
+        Form frm = new ThongBao();
         public Form1()
         {
             InitializeComponent();
         }
 
-       
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            String tk =textboxusername.Text, mk=textboxmatkhau.Text;
+            if (tk ==" "|| mk =="")
+            {
+                MessageBox.Show("vuilong dien tai khoan mk");
+            }
+            if (tk == "admin" && mk == "admin")
+            {
+                this.Hide();
+                frm.ShowDialog();
+            }
+                
+        }
     }
 }
